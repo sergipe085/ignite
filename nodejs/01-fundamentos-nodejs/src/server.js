d22b56d0359280ef85239411b6536e20432c1817
sergipe085 <dapscreed@gmail.com>
2023-08-12 21:00:56 -0300

    making the http requests with stream

--- nodejs/01-fundamentos-nodejs/src/server.js
+++ nodejs/01-fundamentos-nodejs/src/server.js
@@ -2,21 +2,33 @@ import http from "http"
 
 const users = []
 
-const httpServer = http.createServer((req, res) => {
+const httpServer = http.createServer(async (req, res) => {
     const { url, method } = req;
+
+    const chunks = [];
+
+    for await (var chunk of req) {
+        console.log(chunk)
+        chunks.push(chunk);
+    }
+    try {
+        req.body = JSON.parse(Buffer.concat(chunks).toString());
+    }
+    catch {
+        req.body = null
+    }
 
     if (method == "GET" && url == "/users") {
         return res.setHeader("Content-type", "application/json").end(JSON.stringify(users));
     }
 
     if (method == "POST" && url == "/users") {
+        const { name, email } = req.body;
+
         users.push({
-            id: 1,
-            name: "podinha",
-            endereco: {
-                rua: "Rua marcelo gentil porto",
-                bairro: "Luciano cavalcante"
-            }
+            id: users.length + 1,
+            name,
+            email
         })
         
         return res.writeHead(201).end()
